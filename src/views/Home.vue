eafaa39f8dd0e846981184d6114902f79b8bc3c4
Radu <manta.raduwd@gmail.com>
2021-12-27 00:57:12 +0100

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>
+      Hey ich bin Radu und das ist mein Bodycount:
+    </h1>
+    <h1>
+      {{ fuckCounter }}
+    </h1>
+    <button @click="sendFuck()">
+      Fick mich!
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      fuckCounter: 0,
+    }
+  },
+  methods: {
+    sendFuck() {
+      this.fuckCounter += 1;
+    }
+  }
+}
+</script>
+
